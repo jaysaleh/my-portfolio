43927374bb99a7a6cdd6affc44a1bfa4d51ed449
Jay Saleh <jaysaleh66@gmail.com>
2020-06-30 21:14:21 +0000

    moved todo

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// TODO: Modify servlet to access multiple messages from servlet.
-// TODO: Convert comment List to ImmutableList
-
 package com.google.sps.servlets;
 
 import java.util.*; 
@@ -25,8 +22,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 /** Servlet that writes an list of messages as a response. */
+/* 
+ * TODO: Modify servlet to access multiple messages from servlet.
+ * TODO: Convert comment List to ImmutableList
+ */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
   
