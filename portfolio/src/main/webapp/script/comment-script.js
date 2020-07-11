20b20a5bb33374018a5a162867513c67ba3cc116
Jay Saleh <jaysaleh66@gmail.com>
2020-07-11 01:44:22 +0000

    Fixed comments

--- portfolio/src/main/webapp/script/comment-script.js
+++ portfolio/src/main/webapp/script/comment-script.js
@@ -41,24 +41,25 @@ async function getUserLoginData() {
   var lineBreak = document.getElementById('line');
   var deleteButton = document.getElementById('delete-button');
 
-  /** Hide/show containers depending on user login state */
+  /* Hide/show containers depending on user login state */
   if(userData.loggedIn) {
     loginButtonContainer.style.display = states.HIDE;
     commentForm.style.display = states.SHOW;
     deleteButton.style.display = states.SHOW;
     lineBreak.style.display = states.SHOW;
     
-    /** Sets the logout link */
+    /* Sets the logout link */
     logoutButtonForm.action = userData.logoutURL;
-  } else {
-    loginButtonContainer.style.display = states.SHOW;
-    commentForm.style.display = states.HIDE;
-    deleteButton.style.display = states.HIDE;
-    lineBreak.style.display = states.HIDE;
-
-    /** Sets the login link */
-    loginButtonForm.action = userData.loginURL;
+    return;
   }
+  
+  loginButtonContainer.style.display = states.SHOW;
+  commentForm.style.display = states.HIDE;
+  deleteButton.style.display = states.HIDE;
+  lineBreak.style.display = states.HIDE;
+
+    /* Sets the login link */
+  loginButtonForm.action = userData.loginURL;
 }
 
 /**
