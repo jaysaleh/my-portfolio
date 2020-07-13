0d5530fcb84a7f530346636c159587ed1e89ff11
Jay Saleh <jaysaleh66@gmail.com>
2020-07-13 23:50:23 +0000

    bug fixed

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -56,7 +56,7 @@ public class DataServlet extends HttpServlet {
   private static final String IMAGE_URL = "imageURL";
 
   // Supported image files.
-  private static final String JPEG = "image/jpg";
+  private static final String JPEG = "image/jpeg";
   private static final String PNG = "image/png";
   private static final String TIFF = "image/tiff";
 
