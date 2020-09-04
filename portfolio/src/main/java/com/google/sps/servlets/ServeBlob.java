45bae6ad98170183af7ac0fc5821f4068381936f
Jay Saleh <jaysaleh66@gmail.com>
2020-09-04 00:37:59 +0000

    Removing extra import

--- portfolio/src/main/java/com/google/sps/servlets/ServeBlob.java
+++ portfolio/src/main/java/com/google/sps/servlets/ServeBlob.java
@@ -26,7 +26,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Enumeration;
 
 /** Servlet that respondes with a blob. */
 @WebServlet("/blob")
